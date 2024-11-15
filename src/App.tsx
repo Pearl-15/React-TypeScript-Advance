afcfcc34c27686093187fc2c9e6f79cea68aae53
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-15 16:08:46 +0800

    chore: refactor Button by not passing el

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
     <h1>Let's get started!</h1>
     <Input label="Name" id="name" type="text"></Input>
     <Input label="Age" id="age" type="number"></Input>
-    <Button el="anchor" href="https://google.com">Google Link</Button>
-    <Button el="button" type="button">Button to Click</Button>
+    <Button href="https://google.com">Google Link</Button>
+    <Button type="button">Button to Click</Button>
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,29 @@
 import { ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = {
-    el: 'button'
-} & ComponentPropsWithoutRef<'button'>;
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+    href?: never; 
+    // href? means optional no need to pass as a props if pass then value is nothing never
+};
 
-type AnchorProps = {
-    el: 'anchor';
-} & ComponentPropsWithoutRef<'a'>
+type AnchorProps = ComponentPropsWithoutRef<'a'> & {
+    href?: string;
+    // href? means optional no need to pass as a props if pass then value is must be string
+
+}
+
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps{
+    // input parameters and type => (props: ButtonProps | AnchorProps)
+    // output return value and type => props is AnchorProps => means if output return value is "true" then is is a type of "AnchorProps"
+
+    // to check if in the props include "href" props , it's like to find key "href" in the "props"
+    return 'href' in props;
+};
+
 
 export default function Button( props: ButtonProps | AnchorProps){
 
-    // link element
-    if(props.el === 'anchor'){
+    // link element - so here typescript will automatically know if the "props" are "anchor" props then it will return anchor link
+    if(isAnchorProps(props)){
         return <a className="button" {...props}></a> // destructure props to catch built in/default anchor html props
     }
 
