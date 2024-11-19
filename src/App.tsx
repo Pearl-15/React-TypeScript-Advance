8aabad6351c07e22551ea9a8810889109385187d
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-19 15:55:13 +0800

    Exposing component API with useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import './App.css'
 import Button from './components/Button'
-import Form from './components/Form'
+import Form, { type FormHandle } from './components/Form'
 import Input from './components/Input'
+import { useRef } from 'react';
 
 function App() {
+
+  // calling ForHandle from another component
+  const customForm = useRef<FormHandle>(null);
   
   function handleSave(data: unknown){
     const extractedData = data as {name: string, age: number};
     console.log("extractedData : ", extractedData);
+
+    // clear the form by callling another component method
+    customForm.current?.clear();
   }
 
   return (
     <>
     <h1>Let's get started!</h1>
-    <Form onSave={handleSave}>
+    <Form onSave={handleSave} ref={customForm}>
       <Input label="Name" id="name" type="text"></Input>
       <Input label="Age" id="age" type="number"></Input>
       <Button>Save</Button>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,30 @@
-import { ComponentPropsWithoutRef, type FormEvent } from "react"
+import { ComponentPropsWithoutRef, type FormEvent, useImperativeHandle, useRef, forwardRef } from "react"
+
+export type FormHandle = {
+    clear: () => void;
+}
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value: unknown) => void; // we don't in advance type of "onSave"
 };
 
-export default function Form( {onSave, children, ...otherProps}: FormProps){
+const Form = forwardRef<FormHandle, FormProps>(function Form( 
+    {onSave, children, ...otherProps}, 
+    ref){
+
+    const form = useRef<HTMLFormElement>(null);   // ref for form
+    
+    // clear form
+    useImperativeHandle(ref, () => {
+        return{
+            // return method
+            clear(){
+                console.log("CLEARING .... ");
+                // if current have value then reset the form
+                form.current?.reset();
+            }
+        }
+    });
 
     function handleSubmit(event: FormEvent<HTMLFormElement>){
         event.preventDefault();
@@ -14,12 +34,18 @@ export default function Form( {onSave, children, ...otherProps}: FormProps){
 
         const data = Object.fromEntries(formData);
         console.log("data : ", data);
+
+        onSave(data);  
         
-        onSave(data);    
+        // reset the form (Expose clear form method / api so that parent component can call)
+
+
     }
     return(
-        <form {...otherProps} onSubmit={handleSubmit}>
+        <form {...otherProps} onSubmit={handleSubmit} ref={form}>
           {children}
         </form>
     )
-}
+})
+
+export default Form;
