388657a0509ece755fd520f4e4e6edeec3272e57
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-19 11:08:56 +0800

    feat: use forwardRef pass ref to custome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Button from './components/Button'
 import Input from './components/Input'
+import { useRef } from 'react';
 
 function App() {
   
+  const input = useRef<HTMLInputElement>(null);
 
   return (
     <>
     <h1>Let's get started!</h1>
-    <Input label="Name" id="name" type="text"></Input>
+    <Input label="Name" id="name" type="text" ref={input}></Input>
     <Input label="Age" id="age" type="number"></Input>
     <Button href="https://google.com">Google Link</Button>
     <Button type="button">Button to Click</Button>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import {type ComponentPropsWithoutRef } from "react";
+import {type ComponentPropsWithoutRef, forwardRef } from "react";
 
 type InputProps = {
     label: string;
@@ -6,12 +6,17 @@ type InputProps = {
 } & ComponentPropsWithoutRef<'input'>;
 // type of build in html elements "input" 
 
-export default function Input({label, id, ...props}: InputProps){
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+    {label, id, ...props},
+    ref
+){
     // ...props represent existing/build in HTML props that catch from "input" in this case
     return(
         <p>
             <label htmlFor={id}>{label}</label>
-            <input id={id} {...props}></input>
+            <input id={id} {...props} ref={ref}></input>
         </p>
     )
-}
+})
+
+export default Input;
