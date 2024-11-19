7be0b6a5ebc8226c52c6b0df054c390e628ebd04
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-19 16:55:26 +0800

    feat: build a timer app frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import './App.css'
-import Button from './components/Button'
-import Input from './components/Input'
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-  
-
   return (
     <>
-    <h1>Let's get started!</h1>
-    <Input label="Name" id="name" type="text"></Input>
-    <Input label="Age" id="age" type="number"></Input>
-    <Button href="https://google.com">Google Link</Button>
-    <Button type="button">Button to Click</Button>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
