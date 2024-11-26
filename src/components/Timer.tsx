03ad71b131a20f3d400889654f7e9b3fe88b2e67
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-26 22:40:58 +0800

    feat: add in isRunning in useEffect

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState, useRef } from 'react';
-import { type Timer as TimerProps } from '../store/timers-context';
+import { useEffect, useState, useRef, useContext, createContext } from 'react';
+import { useTimersContext, type Timer as TimerProps } from '../store/timers-context';
 import Container from './UI/Container';
 
 
@@ -10,22 +10,37 @@ export default function Timer({name, duration}: TimerProps) {
 
   const [remainingTime, setRemainingTime ] = useState(duration * 1000); // convert duration to ms that's why *1000
 
+  const {isRunning} = useTimersContext();
+
  if (remainingTime <= 0 && interval.current){
   clearInterval(interval.current);
  }
 
   useEffect(() => {
-    const timer = setInterval(function(){
-      setRemainingTime(prevTime => prevTime - 50);
-    }, 50); // every 50s then update the interval
 
-    interval.current = timer;
+    let timer: any;
+
+    if(isRunning){
+      timer = setInterval(function(){
+        setRemainingTime(prevTime =>{
+
+          if(prevTime <= 0){
+            return prevTime
+          }
+           return (prevTime - 50);
+        });
+      }, 50); // every 50s then update the interval
+      interval.current = timer;
+    }else if(interval.current){
+      clearInterval(interval.current);
+    }
+    
 
    return () => {
     clearInterval(timer)
    }
 
-  },[]);
+  },[isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2); // output with 2 decimal places
 
