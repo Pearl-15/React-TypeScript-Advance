0d9f88041fea042e6d45cd15464ba3be9dafca15
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-27 16:16:22 +0800

    feat: added error catching

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import BlogPosts, { BlogPost } from "./components/BlogPosts";
 import { type ReactNode, useEffect, useState } from "react";
 import { get } from "./utils/http";
 import fetchingImg from './assets/data-fetching.png'
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPost = {
   id: number;
@@ -14,21 +15,38 @@ function App() {
 
   const [ fetchedPosts, setFetchPosts] = useState<BlogPost[] | undefined>();
 
+  const [isFetching, setIsFetching] = useState(false);
+
+  const [error, setError] = useState<string>();
+
 
   useEffect(() => {
 
     async function fetchPosts(){
-      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[];
+      setIsFetching(true);
 
-      const blogPosts: BlogPost[] = data.map(rawPost => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
+      try{
+
+        const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[];
+
+        const blogPosts: BlogPost[] = data.map(rawPost => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
+          }
+        });
+        setFetchPosts(blogPosts);
+      }catch(error){
+
+        // check type of error is an object of Error then show the error message
+        if(error instanceof Error){
+          setError(error.message);
         }
-      });
+      }
 
-      setFetchPosts(blogPosts);
+      setIsFetching(false);
+    
     }
 
     fetchPosts();
@@ -36,8 +54,16 @@ function App() {
 
   let content: ReactNode;
 
+  if(error){
+    content = <ErrorMessage text={error}/>
+  }
+
   if(fetchedPosts){
     content =  <BlogPosts posts={fetchedPosts}/>
+  }
+
+  if(isFetching){
+    content = <p id="loading-fallback">Fetching posts ...</p>
   }
 
   return <main>
